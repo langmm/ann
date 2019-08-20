6506057ed9c0613eb1acd4c844b928f8984ac0b9
Meagan Lang <langmm.astro@gmail.com>
2019-08-20 16:15:43 -0500

    Add .gitignore to the list of files ignored for creating the feedstock patch

--- make_diff.sh
+++ make_diff.sh
@@ -1,1 +1,1 @@
-git diff ac64a252438162a7bdedfd87c1059cf4b93b808b HEAD ':(exclude)make_diff.sh' > cmake.patch
+git diff ac64a252438162a7bdedfd87c1059cf4b93b808b HEAD ':(exclude)make_diff.sh' ':(exclude).gitignore' > cmake.patch
